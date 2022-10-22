145def7fffdb357c907ed8548667f45b56fc2a11
Ionescu Paul-Andrei <70650460+R3dPraiseTheSun@users.noreply.github.com>
2022-10-22 21:52:22 +0300

    Update Lab3.py

--- Python/Lab3/Lab3.py
+++ Python/Lab3/Lab3.py
@@ -1,3 +1,5 @@
+import re
+
 #Ex.1:
 def ex1():
     def function(list_a, list_b):
@@ -39,28 +41,102 @@ def ex3():
         return True
     print(compare_dicts({'A':10, 'B':11, 'C':12, 'D0':{'D1':1111, 'D2':22}}, {'A':10, 'B':11, 'C':12,'D0':{'D1':13, 'E':14, 'F':[15,16]}}))
     print(compare_dicts({'A':10, 'B':11, 'C':12,'D0':{'D1':13, 'E':14, 'F':[15,16]}}, {'A':10, 'B':11, 'C':12,'D0':{'D1':13, 'E':14, 'F':[15,16]}}))
-ex3()
+#ex3()
 
 def ex4():
-    print("Work in progress")
-# ex4()
+    def build_xml_element(tag, content, **kwargs):
+        href = kwargs.get("href")
+        _class = kwargs.get("_class")
+        id = kwargs.get("id")
+        return f'<{tag} href={href}_class={_class}id={id}>{content}</{tag}>'
+    
+    print(build_xml_element ("a", "Hello there", href =" http://python.org ", _class =" my-link ", id= " someid "))
+#ex4()
 
 def ex5():
-    print("Work in progress")
-# ex5()
+    def validate_dict(validation_rules: set, targeted_dict: dict):
+        rules_list = []
+        for i in validation_rules: rules_list.append((i[0], i[1:]))
+        rules_list.sort()
+
+        for key, item in targeted_dict.items():
+            key_found = False
+            for rules in rules_list:
+                if key == rules[0]: 
+                    word_list = re.split(', | ', item)
+                    if(
+                        not rules[1][0] in word_list[0] or
+                        not rules[1][-1] in word_list[-1] or
+                        not rules[1][1] in word_list[1:-1]
+                    ): return False, f'Rules broken for {key}'
+                    key_found = True
+                    break
+            if not key_found: return False, 'Key not found in rule set'
+
+        return True
+
+    print(validate_dict(
+        {("key1", "", "inside", ""), ("key2", "start", "middle", "winter")},
+        {"key1": "come inside, it's too cold out", "key3": "this is not valid"}
+    ))
+    print(validate_dict(
+        {("key1", "", "inside", ""), ("key2", "start", "middle", "winter")},
+        {"key1": "inside, it's too cold out", "key3": "this is not valid"}
+    ))
+    print(validate_dict(
+        {("key1", "", "inside", ""), ("key2", "start", "middle", "winter")},
+        {"key1": "come inside, it's too cold out", "key2": "start in the middle of winter"}
+    ))
+#ex5()
 
 def ex6():
-    print("Work in progress")
-# ex6()
+    def func(list_a):
+        duplicate_set = set()
+        duplicate_number = 0
+        for el in set(list_a): 
+            if (list_a.count(el)-1 > 0): 
+                duplicate_set.add(el)
+                duplicate_number += (list_a.count(el)-1)
+        return len(set(list_a)), duplicate_set, duplicate_number
+    print(func([1, 2, 3, 4, 1, 2, 5, 7, 10, 4, 10, 5, 1, 12]))
+#ex6()
 
 def ex7():
-    print("Work in progress")
-# ex7()
+    def func(*sets):
+        operation_dict = {}
+        nr_o_sets = len(sets)
+        for index in range(nr_o_sets):
+            operation_dict[str(sets[index])+' & '+str(sets[(index+1)%nr_o_sets])] = sets[index].intersection(sets[(index+1)%nr_o_sets])
+            operation_dict[str(sets[index])+' | '+str(sets[(index+1)%nr_o_sets])] = sets[index].union(sets[(index+1)%nr_o_sets])
+            operation_dict[str(sets[index])+' - '+str(sets[(index+1)%nr_o_sets])] = sets[index].difference(sets[(index+1)%nr_o_sets])
+            operation_dict[str(sets[(index+1)%nr_o_sets])+'-'+str(sets[index])] = sets[(index+1)%nr_o_sets].difference(sets[index])
+        print(operation_dict)
+
+    print(func({1,2}, {2,3,6}, {4,6,9,11}, {4,5,6}))
+#ex7()
 
 def ex8():
-    print("Work in progress")
-# ex8()
+    def loop(mapping: dict):
+        found_loop = False
+        next_key = 'start'
+        sequence = []
+        while(not found_loop):
+            if(not next_key == mapping.get(next_key) and not mapping.get(next_key) == 'start'):
+                next_key = mapping.get(next_key)
+                sequence.append(next_key)
+            else: found_loop = True
+        return sequence
+
+    print(loop({'start': 'a', 'b': 'a', 'a': '6', '6': 'z', 'x': '2', 'z': '2', '2': '2', 'y': 'start'}))
+    print(loop({'start': 'a', 'b': 'a', 'a': '6', '6': 'z', 'x': '2', 'z': 'x', '2': 'y', 'y': 'start'}))
+#ex8()
 
 def ex9():
-    print("Work in progress")
-# ex9()
+    def my_function(*args, **kwargs):
+        duplicates = []
+        for key, value in kwargs.items():
+            if value in args: duplicates.append(value)
+        return len(duplicates)
+
+    print(my_function(1, 2, 3, 4, x=1, y=2, z=3, w=5))
+ex9()
