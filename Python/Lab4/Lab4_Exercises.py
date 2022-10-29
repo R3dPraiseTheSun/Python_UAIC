36512e864cebab88d31b6d6d59f35697d10740a2
Ionescu Paul-Andrei <70650460+R3dPraiseTheSun@users.noreply.github.com>
2022-10-29 18:32:59 +0300

    lab4 done

--- Python/Lab4/Lab4_Exercises.py
+++ Python/Lab4/Lab4_Exercises.py
@@ -1,10 +1,37 @@
 import os
+import sys
 import Lab4_Functions as l4
 
 print("Ex1:")
-print(l4.ex_1(os.getcwd() + '\OneDrive\Documente\GitHub\Python_UAIC\Python\Lab4\Test_Directory'))
-print("\nEx2:")
+print(l4.ex_1(os.getcwd()+ '\\Test_Directory'))
+
+print("\nEx2: See New Text Document(2).txt from Test_Directory")
 l4.ex_2(
-    os.getcwd() + '\OneDrive\Documente\GitHub\Python_UAIC\Python\Lab4\Test_Directory',
-    os.getcwd() + '\OneDrive\Documente\GitHub\Python_UAIC\Python\Lab4\Test_Directory\\New Text Document(2).txt'
+    os.getcwd() + '\\Test_Directory',
+    os.getcwd() + '\\Test_Directory\\New Text Document(2).txt'
     )
+
+print("\nEx.3: Folder input")
+print(l4.ex_3(os.getcwd()))
+
+print("\nEx.3: File input")
+print(l4.ex_3(os.getcwd() + '/Test_Directory/New Text Document(2).txt'))
+
+if len(sys.argv) > 1:
+    print("\nEx4:")
+    print(l4.ex_4())
+else: print("\nEx4: argument required!")
+
+print("\nEx5: Folder input")
+print(l4.ex_5(os.getcwd(), 'ananas'))
+print("\nEx5: File input")
+print(l4.ex_5(os.getcwd() + '/Test_Directory/New Text Document(2).txt', 'ananas'))
+
+print("\nEx6:")
+l4.ex_6(os.getcwd() + '/Test_Dire', 'ananas', l4.error_callback)
+
+print("\nEx7")
+print(l4.ex_7(os.getcwd() + '/Test_Directory/New Text Document(2).txt'))
+
+print("\nEx8")
+print(l4.ex_8(os.getcwd()))
--- Python/Lab4/Lab4_Functions.py
+++ Python/Lab4/Lab4_Functions.py
@@ -1,4 +1,7 @@
+from genericpath import isdir
 import os
+import sys
+import numpy as np
 
 def ex_1(given_path):
     return sorted({os.path.splitext(f)[-1] for f in os.listdir(given_path) if os.path.splitext(f)[-1] != ''})
@@ -6,3 +9,73 @@ def ex_1(given_path):
 def ex_2(directory_path, file_path):
     with open(file_path, "w") as fd:
         [fd.write(os.path.abspath(f) + '\n') for f in os.listdir(directory_path) if f.startswith("a") and os.path.splitext(f)[-1] != '']
+
+file_list = []
+def ex_3(my_path):
+    if(os.path.isdir(my_path)):
+        extensions_list = []
+        for file in os.listdir(my_path):
+            if(os.path.splitext(file)[-1] != ''):
+                extensions_list.append(os.path.splitext(file)[-1])
+            else: ex_3(my_path + '/' + file)
+        for ext in np.unique(extensions_list):
+            #print(f'In path {my_path} the extension {ext} appears {extensions_list.count(ext)} times')
+            file_list.append((ext, extensions_list.count(ext)))
+            file_list.sort(key= lambda x: x[1], reverse=True)
+    else:
+        with open(my_path, "r") as fd:
+            return fd.read()[-20:]
+    return file_list
+
+def ex_4():
+    directory_path = sys.argv[1]
+    return {os.path.splitext(f)[-1][1:] for f in os.listdir(directory_path) if not os.path.isdir(f)}
+
+file_list5 = []
+def ex_5(target, to_search):
+    if(os.path.isdir(target)):
+        for file in os.listdir(target):
+            if(os.path.splitext(file)[-1] != ''):
+                if to_search in str(file):
+                    #print(f'{file} found')
+                    file_list5.append(file)
+                else: ex_5(target + '/' + file, to_search)
+            elif('__pycache__' not in file): ex_5(target + '/' + file, to_search)
+    elif(os.path.isfile(target)):
+        with open(target, "r") as fd:
+            if to_search in fd.read():
+                return os.path.abspath(target)
+    else: raise ValueError(target + ': is not valid')
+    return file_list5
+
+def error_callback(exception: Exception):
+    print(exception)
+
+def ex_6(target, to_search, callback):
+    try:
+        ex_5(target, to_search)
+    except Exception as e:
+        callback(e)
+
+def ex_7(file_path):
+    try:
+        if os.path.isdir(file_path):
+            raise Exception("Path should point to a file")
+        return {
+            "full_path": os.path.abspath(file_path),
+            "file_size": os.path.getsize(file_path),
+            "file_extension": os.path.splitext(file_path)[-1][1:],
+            "can_read": os.access(file_path, os.R_OK),
+            "can_write": os.access(file_path, os.W_OK),
+        }
+    except Exception as e:
+        print(e)
+
+def ex_8(dir_path):
+    try:
+        if os.path.isfile(dir_path):
+            raise Exception("Path should point to a directory")
+        return [os.path.abspath(f) for f in os.listdir(dir_path)]
+    except Exception as e:
+        print(e)
+
