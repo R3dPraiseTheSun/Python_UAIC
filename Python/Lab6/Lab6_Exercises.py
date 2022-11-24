1e21321d9ea5670935b95c4030917f18f9bbc5f3
Ionescu Paul-Andrei <70650460+R3dPraiseTheSun@users.noreply.github.com>
2022-11-24 18:45:04 +0200

    Lab6 Final

--- Python/Lab6/Lab6_Exercises.py
+++ Python/Lab6/Lab6_Exercises.py
@@ -1,33 +1,26 @@
 from Lab6_Functions import extract_words, ex_2, ex_3, ex_4, ex_5, ex_6, ex_7, ex_8
 
 
-Ex_1 = None
-print(extract_words("text, ana, are mere"))
+# Ex_1
+print('Ex1:', extract_words("text, ana, are mere"))
 
+# Ex_2
+print('Ex2:', ex_2(r"\w+", "text, ana, are mere", 3))
 
-Ex_2 = None
-print(ex_2(r"\w+", "text, ana, are mere", 3))
+# Ex_3
+print('Ex3:',ex_3(["text, are mere", "ana, 0753355294"], [r"\b\w{3}\b", r"075\d{7}", ]))
 
+# Ex_4
+print('Ex4:',ex_4("example.xml", {"class": "url", "name": "url-form", "data-id": "item"}))
 
-Ex_3 = None
-print(ex_3("text, ana, are mere", [r"\w\w\w", r"mere"]))
+# Ex_5
+print('Ex5:',ex_5("example.xml", {"class": "url", "name": "url-form", "data-id": "item"}))
 
+# Ex_6
+print('Ex6:',ex_6("absefgei ara ara cha"))
 
-Ex_4 = None
-print(ex_4("example.xml", {"class": "url", "name": "url-form", "data-id": "item"}))
+# Ex_7
+print('Ex7:',ex_7("1730110155203"))
 
-
-Ex_5 = None
-print(ex_5("example.xml", {"class": "url", "name": "url-form", "data-id": "item"}))
-
-
-Ex_6 = None
-print(ex_6("absefgei ara ara cha"))
-
-
-Ex_7 = None
-print(ex_7("1730110155203"))
-
-
-Ex_8 = None
-print(ex_8(".", ".*example.*"))
+# Ex_8
+print('Ex8:',ex_8(".", ".*example.*"))
--- Python/Lab6/Lab6_Functions.py
+++ Python/Lab6/Lab6_Functions.py
@@ -7,7 +7,7 @@ def extract_words(input_text: str) -> List[str]:
     Write a function that extracts the words from a given text as a parameter. A word is defined as a sequence of
     alpha-numeric characters.
     """
-    return re.findall(r"[a-zA-Z0-9]+", input_text)
+    return re.findall(r"\w+", input_text)
 
 
 def ex_2(regex: str, input_text: str, x: int) -> List[str]:
@@ -18,17 +18,18 @@ def ex_2(regex: str, input_text: str, x: int) -> List[str]:
     return [i for i in re.findall(regex, input_text) if len(i) == x]
 
 
-def ex_3(input_text: str, regex_list: List[str]) -> List[str]:
+def ex_3(input_text_list: list, regex_list: List[str]) -> List[str]:
     """
-    Write a function that receives as a parameter a string of text characters and a list of regular expressions and
-    returns a list of strings that match on at least one regular expression given as a parameter.
+    Write a function that receives two parameters: a list of strings and a list of regular expressions.
+    The function will return a list of the strings that match on at least one regular expression from the list given as parameter.
     """
     match_list = []
-    for i in regex_list:
-        x = re.findall(i, input_text)
-        for j in x:
-            if j not in match_list:
-                match_list.append(j)
+    for input_text in input_text_list:
+        for regex_el in regex_list:
+            found_elements = re.findall(regex_el, input_text)
+            for element in found_elements:
+                if element not in match_list:
+                    match_list.append(element)
     return match_list
 
 
@@ -43,7 +44,7 @@ def ex_4(path: str, attributes: dict) -> List[str]:
         search_string = r"(<(\w+)" + r"".join(
             [" {key}=\"{value}\"".format(key=key, value=value) for key, value in attributes.items()]
         ) + r">[^</\2>]*</\2>)"
-        print(search_string)
+        #print(search_string)
         match_list += [x[0] for x in re.findall(search_string, xml_data)]
     return match_list
 
@@ -59,7 +60,7 @@ def ex_5(path: str, attributes: dict) -> List[str]:
         search_string = r"(<(\w+) [^>]*(" + r"|".join(
             ["{key}=\"{value}\"".format(key=key, value=value) for key, value in attributes.items()]
         ) + r")[^>]*>[^(<\2>)]*</\2>)"
-        print(search_string)
+        #print(search_string)
         match_list += [x[0] for x in re.findall(search_string, xml_data)]
     return match_list
 
@@ -124,7 +125,7 @@ def ex_7(input_text: str) -> bool:
     control_digit = get_control_digit(input_text[:-1])
     regex_string = r"^" + match_first_digit + match_year + match_month + match_day + match_county + random_digits + \
                    control_digit + r"$"
-    print(regex_string)
+    #print(regex_string)
     return bool(re.match(regex_string, input_text))
 
 
